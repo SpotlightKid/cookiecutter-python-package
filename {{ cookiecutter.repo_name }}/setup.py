01ab7c444b06ffa1f64494054a756cd707ea19b4
Christopher Arndt <chris@chrisarndt.de>
2017-03-02 18:27:10 +0100

    Provide custom distutils test command to run tox
    Signed-off-by: Christopher Arndt <chris@chrisarndt.de>

--- {{ cookiecutter.repo_name }}/setup.py
+++ {{ cookiecutter.repo_name }}/setup.py
@@ -1,10 +1,44 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from __future__ import unicode_literals
+
+import distutils
+import subprocess
+from os.path import dirname, join
+
 from setuptools import setup, find_packages
 
 
-exec(open('{{ cookiecutter.package_name }}/version.py').read())
+def read(*args):
+    return open(join(dirname(__file__), *args)).read()
+
+
+class ToxTestCommand(distutils.cmd.Command):
+    """Distutils command to run tests via tox with 'python setup.py test'.
+
+    Please note that in this package configuration tox uses the dependencies in
+    ``requirements/dev.txt``, the list of dependencies in ``tests_require`` in
+    ``setup.py`` is ignored!
+
+    See https://docs.python.org/3/distutils/apiref.html#creating-a-new-distutils-command
+    for more documentation on custom distutils commands.
+    """
+    description = "Run tests via 'tox'."
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        self.announce("Running tests with 'tox'...", level=distutils.log.INFO)
+        return subprocess.call(['tox'])
+
+
+exec(read('{{ cookiecutter.package_name }}', 'version.py'))
 
 classifiers = """
 # The next line is important: it prevents accidental upload to PyPI!
@@ -34,7 +68,7 @@ setup(
     name='{{ cookiecutter.project_name }}',
     version=__version__,  # noqa
     description='{{ cookiecutter.project_description }}',
-    long_description=open('README.rst').read(),
+    long_description=read('README.rst'),
     author='{{ cookiecutter.author_name }}',
     author_email='{{ cookiecutter.author_email }}',
     url='{{ cookiecutter.project_url }}',
@@ -43,6 +77,8 @@ setup(
                  if c.strip() and not c.startswith('#')],
     packages=find_packages(exclude=['tests']),
     include_package_data=True,
-    test_suite='tests',
     install_requires=install_requires,
+    cmdclass={
+        'test': ToxTestCommand,
+    }
 )
